b834bf8c000951e14ee53692fcdcac949007b711
Mohammed Shabaaz <mohammedshabaaz0199@gmail.com>
2025-05-17 15:42:31 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.RenderingHints;
 
 public class Calculator extends JFrame {
     private JTextField display;
